b2842c3bca7b142d4f2f1f3d64ba2e7b50005a6f
rocketbro <asherpope@gmail.com>
2025-04-03 04:41:42 -0400

    change blog title

--- next-js/src/components/blog/BlogHero.tsx
+++ next-js/src/components/blog/BlogHero.tsx
@@ -10,8 +10,8 @@ const BlogHero = ({ note }: BlogHeroProps) => {
   return (
     <div className="flex flex-col gap-4">
     <PageHero
-      title="Understanding AI"
-      description="Intrinsic Labs is invested in facilitating widespread, deep comprehension of AI behavior."
+      title="Exploring AI"
+      description="The Latent Spaces team is invested in facilitating widespread, deep understanding of AI behavior."
       bottomPadding={false}
     />
     {note && <h2 className="text-2xl font-bold mx-auto">{note}</h2>}
