ab08032701270a83c3fd6c1a8c8f52b1bb31a7d9
rocketbro <asherpope@gmail.com>
2025-04-17 16:33:33 -0400

    switch home button to fundraiser

--- next-js/src/components/latent-spaces/HomeHero.tsx
+++ next-js/src/components/latent-spaces/HomeHero.tsx
@@ -74,10 +74,10 @@ const HomeHero = () => {
           transition={{ delay: 0.1, duration: 0.5 }}
         >
           <a 
-            href={loomInterface} 
+            href="/fundraising" 
             className="inline-block hover:bg-ls-accent bg-ls-accent/30 backdrop-blur-md border hover:border-ls-accent border-ls-accentLight text-white py-3 w-full max-w-[325px]md:max-w-[400px] font-calling-code text-[1.1rem] rounded-full cursor-pointer transition-all duration-300 text-center mb-12 max-w-xl"
           >
-            See How It Works
+            Contribute Now
           </a>
         </motion.div>
         </div>
